67d1ac369b6430f9b1f0da9ca3065bd82a028f1b
Gaurav Ojha <your_actual_github_gauravojhavaranasi@gmail.com>
2026-04-30 21:16:15 +0530

    Final deploy fix

--- backend/src/app.js
+++ backend/src/app.js
@@ -53,7 +53,7 @@ app.use((err, req, res, next) => {
 
 const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
-  console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on ${PORT}`);
 });
 
 module.exports = app;
